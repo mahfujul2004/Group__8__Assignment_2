06e798f2f8e3da8ace741fe6ea87bc19b5aa0b1c
mahfujul2004 <mahfujulislslam@gmail.com>
2025-08-02 22:24:10 +0600

    Update problem__2.c

--- problem__2.c
+++ problem__2.c
@@ -4,6 +4,7 @@ int main()
 {
     float angle=45, m=1, ratio;
     ratio = m / sin(angle * (3.1416 / 180));
-    printf("The ratio of Slid width and Wavelength is %.2f",ratio);
+    printf("The ratio of Slit width and Wavelength is %.2f\n",ratio);
     return 0;
 }
+
